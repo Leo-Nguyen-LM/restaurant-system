c536e56de79f707b5803c40c5ff56f904f8e96a3
Leo Nguyen <79421819+Leo-Nguyen-LM@users.noreply.github.com>
2023-05-08 15:19:04 +0100

    Add files via upload

---
+++ src/main/java/Login/Web/LoginServlet.java
@@ -0,0 +1,46 @@
+package Login.Web;
+
+import java.io.IOException;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+import Login.Database.LoginDatabase;
+import Login.Bean.LoginBean;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = -5947371781560086598L;
+	private LoginDatabase LoginDatabase;
+
+    public void init() {
+    	LoginDatabase = new LoginDatabase();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        LoginBean loginBean = new LoginBean();
+        loginBean.setUsername(username);
+        loginBean.setPassword(password);
+        System.out.println(username);
+        System.out.println(password);
+        
+        try {
+            if ( LoginDatabase.validate(loginBean)) {
+                //HttpSession session = request.getSession();
+                // session.setAttribute("username",username);
+                response.sendRedirect("loginsuccess.jsp");
+            } else {
+                //HttpSession session = request.getSession();
+                //session.setAttribute("user", username);
+                response.sendRedirect("login.jsp");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
